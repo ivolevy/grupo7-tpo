34ca8d99d1b720042e8a7cb7b242358340489c48
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2024-03-25 16:24:33 -0300

    adding members section

--- src/assets/components/home/Members.jsx
+++ src/assets/components/home/Members.jsx
@@ -1,4 +1,16 @@
 import { FaArrowRight } from "react-icons/fa";
+import g203 from '../../img/g203.png'
+
+const MembersSquareCont = ({ imgSrc, text }) => {
+    return (
+        <div className="membersSquare">
+            <div className="membersImgContainer">
+                <img src={imgSrc} alt="" className="membersImg" />
+            </div>
+            <p className="membersText">{text}</p>
+        </div>
+    )
+}
 
 export const Members = () => {
     return (
@@ -7,6 +19,12 @@ export const Members = () => {
               <h2 className="productsTitle">members</h2>
               <p className="productsTitleSc">We proudly introduce our powerful members</p>
               <p className="productsText">Find more <FaArrowRight className="icon"/></p>
+              <div className="membersContainer">
+                <MembersSquareCont imgSrc={g203} text='hola'/>
+                <MembersSquareCont imgSrc={g203} text='hola'/>
+                <MembersSquareCont imgSrc={g203} text='hola'/>
+                <MembersSquareCont imgSrc={g203} text='hola'/>
+              </div>
           </section>
       </>
     )
