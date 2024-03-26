121d99d580370337ca70adb0cad6a1019a2616df
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2024-03-26 16:44:09 -0300

    navbar styles

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
@@ -9,24 +9,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/Nav.css';
 
 export const CustomNav = () => {
+  const [activeLink, setActiveLink] = useState('home');
+
+  const handleNavLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" id="nav" sticky="top">
       <Container>
         <Navbar.Brand href="#home">
           <img
             src={logo}
-            width="120"
+            width="110"
             height="auto"
-            className="d-inline-block align-top"
+            className="d-inline-block align-top logoNav"
             alt="React Bootstrap logo"
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={`navItem ${activeLink === 'home' ? 'active' : ''}`}
+              onClick={() => handleNavLinkClick('home')}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#features"
+              className={`navItem ${activeLink === 'features' ? 'active' : ''}`}
+              onClick={() => handleNavLinkClick('features')}
+            >
+              Features
+            </Nav.Link>
+            <Nav.Link
+              href="#pricing"
+              className={`navItem ${activeLink === 'pricing' ? 'active' : ''}`}
+              onClick={() => handleNavLinkClick('pricing')}
+            >
+              Pricing
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -36,10 +60,10 @@ export const CustomNav = () => {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Button variant="outline-success">Search</Button>
+            <Button id='navButton'>Contact</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
