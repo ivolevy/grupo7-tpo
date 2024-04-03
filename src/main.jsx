a58d256508b9c043c1e05d32b5d8fce273076dee
Ivan Levy <ivanlevy@MacBook-Air-de-Ivan.local>
2024-04-03 11:52:11 -0300

    react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {Home} from './Home'
-import './assets/css/Main.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './Home';
+import { Products } from './Products';
+import './assets/css/Main.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/productos" element={<Products />} />
+    </Routes>
+  </BrowserRouter>,
+);
