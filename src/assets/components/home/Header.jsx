04ac1d92a2428e320bf695d0ebf10fe77a4fb24e
Iñaki Moreno <99410656+Inakimo@users.noreply.github.com>
2024-04-14 10:21:33 -0300

    Tailwind

--- src/assets/components/home/Header.jsx
+++ src/assets/components/home/Header.jsx
@@ -1,28 +1,31 @@
-import g502 from '../../img/logitech-g502.png'
+import g502 from "../../img/logitech-g502.png";
 
 export const Header = () => {
   return (
     <header className="header">
-          <div className='leftHeader'>
-            <h1 className='headerInnerTitle'>Tech products</h1>
-            <h2 className="headerTitle">g502 hero wireless</h2>
-            <p className="headerSubtitle">logitech high performance wireless gaming mouse</p>
-            <p className="headerDescription">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, doloribus vitae fugit rem rerum 
-                alias esse possimus, ex iusto totam dolorem aut veritatis nulla nesciunt! Nemo, provident eum 
-                iste delectus.
-            </p>
-            <p className="headerPrice">usd 99.99</p>
-            <div className="headerButtons">
-                <button className="button button1">Add to cart</button>
-                <button className="button button2">See details</button>
-            </div>
-          </div>
-          <div className="rightHeader">
-            <div className='imgHeaderContainer'>
-                <img src={g502} alt="" id="headerImg"/>
-            </div>
-          </div>
-      </header>
-  )
-}
+      <div className="leftHeader">
+        <h1 className="headerInnerTitle">Tech products</h1>
+        <h2 className="headerTitle">g502 hero wireless</h2>
+        <p className="headerSubtitle">
+          logitech high performance wireless gaming mouse
+        </p>
+        <p className="headerDescription">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+          doloribus vitae fugit rem rerum alias esse possimus, ex iusto totam
+          dolorem aut veritatis nulla nesciunt! Nemo, provident eum iste
+          delectus.
+        </p>
+        <p className="headerPrice">usd 99.99</p>
+        <div className="headerButtons">
+          <button className="button button1">Add to cart</button>
+          <button className="button button2">See details</button>
+        </div>
+      </div>
+      <div className="rightHeader">
+        <div className="imgHeaderContainer">
+          <img src={g502} alt="" id="headerImg" />
+        </div>
+      </div>
+    </header>
+  );
+};
