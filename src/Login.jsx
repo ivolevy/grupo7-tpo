173e32acc0e018760142317b64c00d6494e714cc
Iñaki Moreno <99410656+Inakimo@users.noreply.github.com>
2024-04-27 11:47:35 -0300

    Acomodo de componentes

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,12 @@
 import { CustomNav } from "./assets/components//Nav";
-import { LoginForm } from "./assets/components/LoginForm";
+import { LoginForm } from "./assets/components/login/LoginForm";
 import "../src/assets/css/Home.css";
 
 export const Login = () => {
-  return (
-    <>
-      <CustomNav />
-      <LoginForm />
-    </>
-  );
+	return (
+		<>
+			<CustomNav />
+			<LoginForm />
+		</>
+	);
 };
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,12 +1,12 @@
 import { CustomNav } from "./assets/components//Nav";
-import { RegisterForm } from "./assets/components/RegisterForm";
+import { RegisterForm } from "./assets/components/register/RegisterForm";
 import "../src/assets/css/Home.css";
 
 export const Register = () => {
-  return (
-    <>
-      <CustomNav />
-      <RegisterForm />
-    </>
-  );
+	return (
+		<>
+			<CustomNav />
+			<RegisterForm />
+		</>
+	);
 };
