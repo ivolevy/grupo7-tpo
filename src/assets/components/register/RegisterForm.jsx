f93d618b161618a1eb758d1dacaf54948e713e27
Olmos2121 <87035570+Olmos2121@users.noreply.github.com>
2024-05-07 19:53:25 -0300

    Registro completo

--- src/assets/components/register/RegisterForm.jsx
+++ src/assets/components/register/RegisterForm.jsx
@@ -1,73 +1,142 @@
 import { useState } from "react";
 
 export function RegisterForm() {
-	const [error, setError] = useState(false);
+    const [error, setError] = useState(false);
+	const [errorEmail, setErrorInvalidEmail] = useState(false);
+	const [registrationSuccess, setRegistrationSuccess] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        lastName: "",
+        email: "",
+        user: "",
+        password: ""
+    });
 
-	function setEmail(e) {
-		if (e.length > 8 && e.includes("@")) {
-			sessionStorage.setItem("email", e);
-		}
-	}
+    function handleChange(e) {
+        const { name, value } = e.target;
 
-	function setUser(e) {
-		if (e.length > 8) {
-			sessionStorage.setItem("user", e);
-		}
-	}
-
-	function setPassword(e) {
-		if (e != "") {
-			sessionStorage.setItem("password", e);
-		}
-	}
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
 
 	function submitForm(e) {
 		e.preventDefault();
-
-		const pass = sessionStorage.getItem("password");
-		const user = sessionStorage.getItem("user");
-		const email = sessionStorage.getItem("email");
-
-		if (pass != "" && user != "" && email != "") {
-			setError(false);
-		} else {
+	
+		const { name, lastName, email, user, password } = formData;
+		let formValid = true;
+	
+		if (!name || name.trim() === "") {
 			setError(true);
+			formValid = false;
+		}
+	
+		if (!lastName || lastName.trim() === "") {
+			setError(true);
+			formValid = false;
+		}
+	
+		if (!email || email.trim() === "") {
+			setError(true);
+			formValid = false;
+		} else if (!email.includes("@")) {
+			setError(false);
+			setErrorInvalidEmail(true);
+			formValid = false;
+		}
+	
+		if (!user || user.trim() === "") {
+			setError(true);
+			formValid = false;
+		}
+	
+		if (!password || password.trim() === "") {
+			setError(true);
+			formValid = false;
+		}
+	
+		if (formValid) {
+			setError(false);
+			setErrorInvalidEmail(false);
+			setRegistrationSuccess(true);
+			sessionStorage.setItem("name", name);
+			sessionStorage.setItem("lastName", lastName);
+			sessionStorage.setItem("email", email);
+			sessionStorage.setItem("user", user);
+			sessionStorage.setItem("password", password);
+
+			setTimeout(() => {
+				window.location.assign("/login");
+			}, 1500);
 		}
 	}
 
 	return (
 		<section className="flex flex-col items-center justify-center">
-			<h1 className="text-3xl font-bold mb-4 text-white">Register</h1>
-			<form className="flex flex-col items-center justify-center">
+			<h1 className="text-3xl font-bold mb-4 text-white relative z-10">Register</h1>
+			<form className="flex flex-col items-center justify-center" onSubmit={submitForm}>
 				<input
 					type="text"
+					name="name"
+					placeholder="Nombre"
+					className="w-64 h-12 px-4 mb-4 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+					value={formData.name}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					name="lastName"
+					placeholder="Apellido"
+					className="w-64 h-12 px-4 mb-4 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
+					value={formData.lastName}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					name="email"
 					placeholder="Email"
 					className="w-64 h-12 px-4 mb-4 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-					onChange={(e) => setEmail(e.target.value)}
-				/>
+					value={formData.email}
+					onChange={handleChange}				/>
 				<input
 					type="text"
+					name="user"
 					placeholder="Usuario"
 					className="w-64 h-12 px-4 mb-4 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-					onChange={(e) => setUser(e.target.value)}
-				/>
+					value={formData.user}
+					onChange={handleChange}				/>
 				<input
 					type="password"
+					name="password"
 					placeholder="Contraseña"
 					className="w-64 h-12 px-4 mb-4 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-400"
-					onChange={(e) => setPassword(e.target.value)}
-				/>
+					value={formData.password}
+					onChange={handleChange}				/>
 				<button
 					type="submit"
 					className="w-64 h-12 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-400"
-					onSubmit={submitForm}
 				>
 					Registrarse
 				</button>
+				{error && (
+				<p className="text-red-600 mt-4">
+					Todos los campos son obligatorios
+				</p>
+				)}
+				{errorEmail && (
+				<p className="text-red-600 mt-2.5">
+					Formato de Email incorrecto
+				</p>
+				)}
 			</form>
-			{error && (
-				<p className="text-red-600 mt-4">Todos los campos son obligatorios</p>
-			)}
+			{registrationSuccess && (
+                <div className="bg-green-200 text-green-700 p-4 rounded-md mt-4">
+                    Registro exitoso
+                </div>
+            )}
+		
+			
 		</section>
 	);
 }
