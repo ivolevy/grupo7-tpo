2c09972d7e1af10bb9e2195c3fb5dadcfa4b1a7d
ivan levy <ivo.levy03@gmail.com>
2024-06-20 13:58:52 -0300

    product with no stock can´t be added to the cart

--- src/Product.jsx
+++ src/Product.jsx
@@ -8,75 +8,68 @@ import { useDispatch } from "react-redux";
 import { addtoCart } from "./redux/reducers/cartSlice";
 
 export const Product = ({ products }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
 
-	const handleCartClick = (event) => {
-		if (event.target.classList.contains("add")) {
-			return true;
-		} else {
-			return false;
-		}
-	};
+  useEffect(() => {
+    const productData = products.find((item) => item.id === parseInt(id));
+    setProduct(productData);
+  }, [id, products]);
 
-	const handleAddToCart = (product) => {
-		dispatch(addtoCart(product));
-	};
+  const handleCartClick = () => {
+    if (product && product.stock > 0) {
+      dispatch(addtoCart(product));
+    } else {
+      // Mostrar mensaje de producto sin stock
+      alert("Producto sin stock");
+    }
+  };
 
-	const { id } = useParams();
-	const [product, setProduct] = useState(null);
-
-	useEffect(() => {
-		const productData = products.find((item) => item.id === parseInt(id));
-		setProduct(productData);
-	}, [id, products]);
-
-	return (
-		<>
-			<Nav />
-			<div className="mx-auto flex flex-col h-[55vh]">
-				{product ? (
-					<div className="flex-1 grid grid-cols-12 gap-4 p-1">
-						<div className="col-span-12 rounded-lg bg-gray-element sm:col-span-8 flex items-center justify-center ">
-							<img
-								src={product.image}
-								className="productImg"
-								alt={product.name}
-							/>
-						</div>
-						<div className="col-span-12 rounded-lg bg-blue-bizio p-16 sm:col-span-4 flex flex-col justify-center">
-							<h1 className="productViewTitle text-blac text-4xl uppercase mb-4">
-								{product.name}
-							</h1>
-							<p className="productViewDesc mb-4 text-gray-element">
-								{product.description}
-							</p>
-							<p className="productViewPrice mb-4 text-black text-3xl">
-								${product.price} USD
-							</p>
-							<div className="productViewButtons">
-								<button
-									className="productViewButton bg-black add"
-									onClick={(event) => {
-										if (handleCartClick(event)) {
-											handleAddToCart(product);
-										} else {
-											return;
-										}
-									}}
-								>
-									Add to cart
-								</button>
-							</div>
-						</div>
-					</div>
-				) : (
-					<p>Producto no encontrado</p>
-				)}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Nav />
+      <div className="mx-auto flex flex-col h-[55vh]">
+        {product ? (
+          <div className="flex-1 grid grid-cols-12 gap-4 p-1">
+            <div className="col-span-12 rounded-lg bg-gray-element sm:col-span-8 flex items-center justify-center">
+              <img
+                src={product.image}
+                className="productImg"
+                alt={product.name}
+              />
+            </div>
+            <div className="col-span-12 rounded-lg bg-blue-bizio p-16 sm:col-span-4 flex flex-col justify-center">
+              <h1 className="productViewTitle text-black text-4xl uppercase mb-4">
+                {product.name}
+              </h1>
+              <p className="productViewDesc mb-4 text-gray-element">
+                {product.description}
+              </p>
+              <p className="productViewPrice mb-4 text-black text-3xl">
+                ${product.price} USD
+              </p>
+              <div className="productViewButtons">
+                <button
+                  className={`productViewButton bg-black add ${
+                    product.stock === 0 ? "disabled" : ""
+                  }`}
+                  onClick={handleCartClick}
+                  disabled={product.stock === 0}
+                >
+                  {product.stock === 0 ? "Sin Stock" : "Add to Cart"}
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <p>Producto no encontrado</p>
+        )}
+      </div>
+    </>
+  );
 };
 
 Product.propTypes = {
-	products: PropTypes.array.isRequired,
+  products: PropTypes.array.isRequired,
 };
