d2a2a7f566e938b9b0f63fd4e89a0bb59247fd60
Olmos2121 <tomyolmos1@hotmail.com>
2024-06-28 19:17:23 -0300

    Ahora se puede agregar productos a la base de datos

--- src/assets/components/admin/CreateProduct.jsx
+++ src/assets/components/admin/CreateProduct.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+import { createProduct } from '../../../api';
 
 export const CreateProduct = ({ categories, onSubmit }) => {
   const [productData, setProductData] = useState({
-    id: '',
     name: '',
     description: '',
     brand: '',
@@ -35,230 +35,209 @@ export const CreateProduct = ({ categories, onSubmit }) => {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    onSubmit(productData);
-    setProductData({
-      id: '',
-      name: '',
-      description: '',
-      brand: '',
-      category: 'Todos',
-      price: '',
-      discounted: false,
-      discountAmount: 0,
-      image: null,
-      stock: '', 
-    });
-  };
 
-  return (
-    <div>
-      <h3 className="text-xl mb-4">Crear Nuevo Producto</h3>
-      <form onSubmit={handleSubmit} className="max-w-lg mx-auto grid grid-cols-2 gap-4">
-        <div>
-          <label
-            htmlFor="productId"
-            className="block text-sm font-medium text-gray-700"
-          >
-            ID
-          </label>
-          <input
-            type="text"
-            id="productId"
-            name="id"
-            value={productData.id}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            required
-          />
-        </div>
+    try {
+      const { name, description, brand, category, price,
+        discounted, discountAmount, image, stock } = productData;
+      const data = await createProduct(name, description, brand, category, price,
+        discounted, discountAmount, image, stock);
+      onSubmit(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        <div>
-          <label
-            htmlFor="productName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Nombre
-          </label>
-          <input
-            type="text"
-            id="productName"
-            name="name"
-            value={productData.name}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            required
-          />
-        </div>
-
-        <div className="col-span-2">
-          <label
-            htmlFor="productDescription"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Descripción
-          </label>
-          <textarea
-            id="productDescription"
-            name="description"
-            value={productData.description}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            rows="3"
-            required
-          ></textarea>
-        </div>
-
-        <div>
-          <label
-            htmlFor="productBrand"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Marca
-          </label>
-          <input
-            type="text"
-            id="productBrand"
-            name="brand"
-            value={productData.brand}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            required
-          />
-        </div>
-
-        <div>
-          <label
-            htmlFor="productCategory"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Categoría
-          </label>
-          <select
-            id="productCategory"
-            name="category"
-            value={productData.category}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            required
-          >
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div>
-          <label
-            htmlFor="productPrice"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Precio
-          </label>
-          <input
-            type="text"
-            id="productPrice"
-            name="price"
-            value={productData.price}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            required
-          />
-        </div>
-
-        <div className="flex items-center">
-          <input
-            id="productDiscounted"
-            name="discounted"
-            type="checkbox"
-            checked={productData.discounted}
-            onChange={handleChange}
-            className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
-          />
-          <label
-            htmlFor="productDiscounted"
-            className="ml-2 block text-sm text-gray-900"
-          >
-            Producto en Descuento
-          </label>
-        </div>
-
-        {productData.discounted && (
-          <div className="w-full">
+    return (
+      <div>
+        <h3 className="text-xl mb-4">Crear Nuevo Producto</h3>
+        <form onSubmit={handleSubmit} className="max-w-lg mx-auto grid grid-cols-2 gap-4">
+          <div>
             <label
-              htmlFor="productDiscountAmount"
+              htmlFor="productName"
               className="block text-sm font-medium text-gray-700"
             >
-              Monto de Descuento (%)
+              Nombre
             </label>
             <input
-              type="number"
-              id="productDiscountAmount"
-              name="discountAmount"
-              value={productData.discountAmount}
+              type="text"
+              id="productName"
+              name="name"
+              value={productData.name}
               onChange={handleChange}
               className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               required
             />
           </div>
-        )}
 
-        <div className="col-span-2">
-          <label
-            htmlFor="productImage"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Imagen
-          </label>
-          <input
-            type="file"
-            id="productImage"
-            name="image"
-            accept="image/*"
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            required
-          />
-          {productData.image && (
-            <img
-              src={URL.createObjectURL(productData.image)}
-              alt="Preview"
-              className="mt-2 rounded-md border border-gray-300"
-              style={{ maxWidth: '200px' }}
+          <div>
+            <label
+              htmlFor="productStock"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Stock
+            </label>
+            <input
+              type="text"
+              id="productStock"
+              name="stock"
+              value={productData.stock}
+              onChange={handleChange}
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              required
             />
+          </div>
+
+          <div className="col-span-2">
+            <label
+              htmlFor="productDescription"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Descripción
+            </label>
+            <textarea
+              id="productDescription"
+              name="description"
+              value={productData.description}
+              onChange={handleChange}
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              rows="3"
+              required
+            ></textarea>
+          </div>
+
+          <div>
+            <label
+              htmlFor="productBrand"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Marca
+            </label>
+            <input
+              type="text"
+              id="productBrand"
+              name="brand"
+              value={productData.brand}
+              onChange={handleChange}
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="productCategory"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Categoría
+            </label>
+            <select
+              id="productCategory"
+              name="category"
+              value={productData.category}
+              onChange={handleChange}
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              required
+            >
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label
+              htmlFor="productPrice"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Precio
+            </label>
+            <input
+              type="text"
+              id="productPrice"
+              name="price"
+              value={productData.price}
+              onChange={handleChange}
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              required
+            />
+          </div>
+
+          <div className="flex items-center">
+            <input
+              id="productDiscounted"
+              name="discounted"
+              type="checkbox"
+              checked={productData.discounted}
+              onChange={handleChange}
+              className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"
+            />
+            <label
+              htmlFor="productDiscounted"
+              className="ml-2 block text-sm text-gray-900"
+            >
+              Producto en Descuento
+            </label>
+          </div>
+
+          {productData.discounted && (
+            <div className="w-full">
+              <label
+                htmlFor="productDiscountAmount"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Monto de Descuento (%)
+              </label>
+              <input
+                type="number"
+                id="productDiscountAmount"
+                name="discountAmount"
+                value={productData.discountAmount}
+                onChange={handleChange}
+                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                required
+              />
+            </div>
           )}
-        </div>
 
-        <div>
-          <label
-            htmlFor="productStock"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Stock
-          </label>
-          <input
-            type="text"
-            id="productStock"
-            name="stock"
-            value={productData.stock}
-            onChange={handleChange}
-            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-            required
-          />
-        </div>
+          <div className="col-span-2">
+            <label
+              htmlFor="productImage"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Imagen
+            </label>
+            <input
+              type="file"
+              id="productImage"
+              name="image"
+              accept="image/*"
+              onChange={handleChange}
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              required
+            />
+            {productData.image && (
+              <img
+                src={URL.createObjectURL(productData.image)}
+                alt="Preview"
+                className="mt-2 rounded-md border border-gray-300"
+                style={{ maxWidth: '200px' }}
+              />
+            )}
+          </div>
 
-        <div className="col-span-2 mt-4">
-          <button
-            type="submit"
-            className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
-          >
-            Crear Producto
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-};
+          <div className="col-span-2 mt-4">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+            >
+              Crear Producto
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
